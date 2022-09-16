f9246f4f8f0caad1ddc575367a0588b32c9ff15b
Guigz15 <91730152+Guigz15@users.noreply.github.com>
2022-09-16 16:07:40 -0400

    Feat: add images ressource and make main window view

--- src/main/java/com/uqac/AspiBotApplication.java
+++ src/main/java/com/uqac/AspiBotApplication.java
@@ -3,10 +3,8 @@ package com.uqac;
 import com.uqac.controller.MainWindowController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Objects;
 
