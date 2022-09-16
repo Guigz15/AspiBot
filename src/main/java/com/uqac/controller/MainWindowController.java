b64ced8ec99b8ef20e7e37ac0f1518ca4997d4f9
Julien <julien.perritaz@etu.univ-tours.fr>
2022-09-16 17:44:01 -0400

    MainWindowController add double List

--- src/main/java/com/uqac/controller/MainWindowController.java
+++ src/main/java/com/uqac/controller/MainWindowController.java
@@ -1,4 +1,39 @@
 package com.uqac.controller;
 
-public class MainWindowController {
+import com.uqac.model.AspiBot;
+import com.uqac.model.Tile;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+public class MainWindowController implements Initializable {
+    @FXML
+    private GridPane gridPane;
+    @Getter @Setter
+    private List<List<Tile>> tiles;
+
+    public void initialize() {
+        for(int i = 0; i < 5; i++) {
+            List<Tile> row = new ArrayList<>();
+            for(int j = 0; j < 5; j++) {
+                Tile tile = new Tile();
+                row.add(tile);
+            }
+            tiles.add(row);
+        }
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
